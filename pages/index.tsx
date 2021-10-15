f8c76a5b594204fd8a0d073bf3e991ca3de298aa
lucasb <lucasb@trin.ca>
2021-10-15 12:43:07 -0300

    responsive content

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,11 @@ import Details from '../src/sections/details'
 
 const IndexPage = () => (
   <Container maxWidth="container.xl" padding={0}>
-    <Flex h="100vh" py={20}>
+    <Flex
+      py={[0, 10, 20]}
+      h={{ base: 'auto', md: '100vh' }}
+      direction={{ base: 'column-reverse', md: 'row' }}
+    >
       <Details />
       <Cart />
     </Flex>
