28dbbfec321aff1b998aa2573279527e20fa4669
lucasnsborges <lucasnsborges@gmail.com>
2021-10-16 18:52:02 -0300

    adjust the indentation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
-import theme from '../src/theme'
-import '../src/theme/styles.css'
+import theme from "../src/theme";
+import "../src/theme/styles.css";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
-import { Container, Flex } from '@chakra-ui/react'
-import Cart from '../src/sections/cart'
-import Details from '../src/sections/details'
+import { Container, Flex } from "@chakra-ui/react";
+import Cart from "../src/sections/cart";
+import Details from "../src/sections/details";
 
 const IndexPage = () => (
   <Container maxWidth="container.xl" padding={0}>
     <Flex
       py={[0, 10, 20]}
-      h={{ base: 'auto', md: '100vh' }}
-      direction={{ base: 'column-reverse', md: 'row' }}
+      h={{ base: "auto", md: "100vh" }}
+      direction={{ base: "column-reverse", md: "row" }}
     >
       <Details />
       <Cart />
     </Flex>
   </Container>
-)
+);
 
-export default IndexPage
+export default IndexPage;
